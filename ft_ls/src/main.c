f330dc9676f102aeb034272dae82402959c11ce5
Yacine Sibous <yacinesibous@gmail.com>
2018-03-30 13:25:09 -0700

    working iterator

--- ft_ls/src/main.c
+++ ft_ls/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ysibous <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 20:22:50 by ysibous           #+#    #+#             */
-/*   Updated: 2018/03/30 10:02:19 by ysibous          ###   ########.fr       */
+/*   Updated: 2018/03/30 13:24:15 by ysibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,40 +30,43 @@ t_file_info		*get_file_info(char *str)
 	struct stat *buff;
 	t_file_info *new;
 	t_file_info *root;
-	DIR			*d;
-	struct dirent	*dir;
+	t_file_info *start;
+	DIR			*d = NULL;
+	struct dirent	*dir = NULL;
 
-	new = init_file_info();
-	root = new;
+	root = init_file_info();
+	start = root;
 	buff = (struct stat *)ft_memalloc(sizeof(struct stat));
 	dir = (struct dirent *)ft_memalloc(sizeof(struct dirent));
-	new->name = ft_strnew(ft_strlen(str));
-	new->name = str;
+	root->name = ft_strnew(ft_strlen(str));
+	lstat(str, buff);
+	root->name = str;
 	if (S_ISDIR(buff->st_mode))
 	{
-		new->is_dir = 1;
-		new->next= new;
+		root->is_dir = 1;
 		d = opendir(str);
 		while ((dir = readdir(d)))
 		{
 			new = init_file_info();
 			new->name = ft_strnew(ft_strlen(dir->d_name));
 			new->name = dir->d_name;
+			lstat(new->name, buff);
 			if (S_ISDIR(buff->st_mode))
 				new->is_dir = 1;
-			new->next = new;
-			new = new->next;
+			root->next = new;
+			root = root->next;
 		}
+		root->next = NULL;
 		closedir(d);
 	}
-	return (root);
+	return (start);
 }
-
+/*
 void	order_f_info_lst(t_file_info **root, t_options *opt)
 {
 	return ;
 }
-
+*/
 void	print_lst_info(t_file_info *root)
 {
 	while (root)
@@ -73,20 +76,35 @@ void	print_lst_info(t_file_info *root)
 	}
 }
 
+void	free_f_info_lst(t_file_info **root)
+{
+	t_file_info *tmp;
+
+	while(*root)
+	{
+		tmp = *root;
+		*root = (*root)->next;
+		free(tmp);
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	int  i;
 	t_file_info *root;
 	t_options *opt;
 	opt = init_options();
-	i = get_options(argc, argv, opt);
+	//i = get_options(argc, argv, opt);
+	i = 1;
 	while (i < argc)
 	{
 		root = get_file_info(argv[i]);
 		print_lst_info(root);
+/*
 		if (opt->t || opt->r)
-			order_f_info_lst(root,opt);
-		/*if (opt->R)
+			order_f_info_lst(&root,opt);
+		if (opt->R)
+	
 		{
 			while (root)
 			{
@@ -94,7 +112,7 @@ int		main(int argc, char **argv)
 					main()
 			}
 		}*/
-		free(root);
+		free_f_info_lst(&root);
 		i++;
 	}
 	printf("l is %d\n", opt->l);
